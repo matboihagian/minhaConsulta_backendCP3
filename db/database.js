ca1784874074bac8a84c147ac56e2cafadd9a41d
mboihagian <mboihagian@gmail.com>
2024-10-25 15:19:27 -0300

    first commit

---
+++ db/database.js
@@ -0,0 +1,35 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./consultas.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Conectado ao banco de dados SQLite.');
+  }
+});
+
+// Criar a tabela de usuários
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL
+  )`);
+
+  // Criar a tabela de consultas
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS consultations (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId INTEGER NOT NULL,
+      date TEXT NOT NULL,
+      doctor TEXT NOT NULL,
+      specialty TEXT NOT NULL,
+      status TEXT NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    )`);
+  });
+});
+
+
+module.exports = db;
